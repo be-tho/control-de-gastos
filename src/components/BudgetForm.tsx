c71c30336f4469d4a20d0e88392757f8e1d082a0
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-03-28 22:58:06 -0300

    reducer budget

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react"
+import { useState, useMemo } from "react"
 
 export default function BudgetForm() {
     const [budget, setBudget] = useState(0)
 
+    // Manejar cambio en el presupuesto
     const handleChange = (e : React.ChangeEvent<HTMLInputElement>) => {
         setBudget(Number(e.target.value))
     }
+
+    // Validar presupuesto
+    const isValidBudget = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
 
   return (
     <form className="space-y-5">
@@ -26,7 +32,8 @@ export default function BudgetForm() {
         <input 
             type="submit" 
             value="Añadir Presupuesto" 
-            className="bg-blue-600 hover:bg-blue-700 cursor-pointer text-white w-full p-2 font-black uppercase rounded-md"
+            className="bg-blue-600 hover:bg-blue-700 cursor-pointer text-white w-full p-2 font-black uppercase rounded-md disabled:opacity-50"
+            disabled={isValidBudget}
         />
     </form>
   )
