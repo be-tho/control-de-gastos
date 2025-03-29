09b80b5d9eca5eb12781d0a6194d862957fe80aa
Albert Villarroel <56642267+be-tho@users.noreply.github.com>
2025-03-28 23:15:45 -0300

    agregando context

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,35 @@
+import { useReducer, createContext, Dispatch, ReactNode } from "react"
+import { budgetReducer, BudgetState, initialState, BudgetAction } from "../reducers/budget-reducer"
+
+// Contexto
+type BudgetContextProps = {
+    state: BudgetState | null
+    dispatch: Dispatch<BudgetAction>
+}
+
+// Props del provider
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+// Crear el contexto
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+// Crear el provider
+export const BudgetProvider = ({ children }: BudgetProviderProps) =>{
+    
+    // Reducer
+    const [state, dispatch] = useReducer(budgetReducer, initialState)
+
+    // Retornar el provider
+    return (
+        <BudgetContext.Provider
+            value={{
+                state: state || null,
+                dispatch
+            }}
+        >
+            {children}
+        </BudgetContext.Provider>
+    )
+}
